b488ef6d6cd61ed3964d6ffdeab800a6c7adc4e5
Guhan <guhan27062002@gmail.com>
2023-06-11 12:27:43 +0530

    authentication and authorization performed

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,6 +25,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
+// file storage
 const storage = multer.diskStorage({
     destination: function(req, file, cd) {
         cb(null, "public/assets");
@@ -30,9 +34,15 @@ const storage = multer.diskStorage({
         cb(null, file.originalname);
     }
 });
-
 const upload = multer({ storage });
 
+// routes with files
+app.post("/auth/register", upload.single("picture"), register);
+
+// routes
+app.use("/auth", authRoutes);
+
+// mongoose setup
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
